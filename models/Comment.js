fd54d655c56efe9490adc10c3f61de4432886c07
Craig Roberts <craig.roberts11@outlook.com>
2023-03-21 06:07:25 +1030

    added comment model and updated seeds

--- models/Comment.js
+++ models/Comment.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
-class Post extends Model {}
+class Comment extends Model {}
 
-Post.init(
+Comment.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -23,14 +23,18 @@ Post.init(
       type: DataTypes.INTEGER,
       allowNull: false,
     },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: "post",
+    modelName: "comment",
   }
 );
 
-module.exports = Post;
+module.exports = Comment;
